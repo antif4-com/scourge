bd2b28c720a4e0d4a91d2a8ca67fd4ca305ca547
eian <eian@antif4.com>
2025-02-14 01:19:40 -0800

    initial checkin
    Just get's the basic structure in place, and probably not even that, but wanted to have a place to start "real" commits from.
    
    I don't know why I sometimes quote for emphasis. But I do.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "net-ssh"
+gem "droplet_kit"
---
+++ hosts/hosts.rb
@@ -0,0 +1,18 @@
+
+
+class DigitalOcean < Thor
+
+  desc "create_instance"
+  def CreateInstance
+
+  end
+
+  desc "list_servers", "Lists the servers at DigitalOcean"
+  def ListServers
+  	
+  end
+
+end
+
+
+
